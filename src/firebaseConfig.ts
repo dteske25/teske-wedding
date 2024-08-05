eac52976029653837a37b8613ead57319e41f0cf
Daric Teske <tesked@outlook.com>
2024-08-04 22:29:16 -0500

    fix lint issues

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBRWBe8OpMY1UqzUzKLQQeusGVM-rTdFMA",
+  authDomain: "teske-wedding-cdfd7.firebaseapp.com",
+  projectId: "teske-wedding-cdfd7",
+  storageBucket: "teske-wedding-cdfd7.appspot.com",
+  messagingSenderId: "914530076104",
+  appId: "1:914530076104:web:1c96c0c9fe1421b445ecfd",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBRWBe8OpMY1UqzUzKLQQeusGVM-rTdFMA",
-  authDomain: "teske-wedding-cdfd7.firebaseapp.com",
-  projectId: "teske-wedding-cdfd7",
-  storageBucket: "teske-wedding-cdfd7.appspot.com",
-  messagingSenderId: "914530076104",
-  appId: "1:914530076104:web:1c96c0c9fe1421b445ecfd"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
